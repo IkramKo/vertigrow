6d3ebe0efbf69e6d3c55018cfd6169e39fdaf789
rami <rami@pop-os.localdomain>
2024-02-03 22:53:34 -0500

    Added add fish button and logic

--- game/objects_characters/fish_bag.gd
+++ game/objects_characters/fish_bag.gd
@@ -1,12 +1,44 @@
-#extends Node2D
-#
-## Load the scene you want to instantiate
-##var child_scene = preload("res://path_to_your_scene.tscn")
-#
-#func _input(event):
-	#if event is InputEventMouseButton and event.pressed:
-		## Instantiate the scene
-		##var new_child = child_scene.instance()
-		#
-		## Add the new instance to the current scene
-		#add_child(new_child)
+extends Node2D
+
+const FISH_SCENE_PATH = "res://objects_characters/fish.tscn"
+var fish_scene = preload(FISH_SCENE_PATH)
+
+# Hierarchy
+var shelf = Node
+var farm = Node
+var tank = Node
+
+# Bounds
+const RIGHT_BOUND_X = 125
+const LEFT_BOUND_X = 75
+const TOP_BOUND_Y = 65
+const BOTTOM_BOUND_Y = 225
+
+func _ready():
+	self.shelf = get_parent()
+	self.farm = shelf.get_parent()
+	self.tank = farm.get_child(1)
+	
+	if self.fish_scene == null:
+		print("Failed to load scene at path: ", FISH_SCENE_PATH)
+		return
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		load_fish()
+
+func load_fish():
+	# Instantiate the new fish scene
+	var new_fish = self.fish_scene.instantiate()
+	
+	# Set the position of the new instance to a random position within bounds
+	new_fish.position = Vector2(randf_range(LEFT_BOUND_X, RIGHT_BOUND_X), randf_range(TOP_BOUND_Y, BOTTOM_BOUND_Y))
+	
+	# Add the new instance to the current scene
+	#add_child(new_fish)
+	
+	# Get shelf
+	var shelf = get_parent()
+	var farm = shelf.get_parent()
+	var tank = farm.get_child(1)
+	tank.add_child(new_fish)
