e5bffaaa9a7429222e1829f2a7243c2a6357b268
rami <rami@pop-os.localdomain>
2024-02-03 23:16:04 -0500

    Fixed click bug

--- game/objects_characters/fish_bag.gd
+++ game/objects_characters/fish_bag.gd
@@ -8,6 +8,7 @@ var shelf = Node
 var farm = Node
 var tank = Node
 
+# BOUNDS NEED TO BE MOVED TO TANK EVENTUALLY THIS SUCKS
 # Bounds
 const RIGHT_BOUND_X = 125
 const LEFT_BOUND_X = 75
@@ -23,13 +24,19 @@ func _ready():
 		print("Failed to load scene at path: ", FISH_SCENE_PATH)
 		return
 
+#func _input(event):
+	#if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
+		#if is_mouse_within_bounds():
+			#load_fish()
+
 func _input(event):
-	if event is InputEventMouseButton and event.pressed:
-		load_fish()
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		if self.get_child(0).get_child(0).get_rect().has_point(to_local(event.position)):
+			load_fish()
 
 func load_fish():
 	# Instantiate the new fish scene
-	var new_fish = self.fish_scene.instantiate()
+	var new_fish = fish_scene.instantiate()
 	
 	# Set the position of the new instance to a random position within bounds
 	new_fish.position = Vector2(randf_range(LEFT_BOUND_X, RIGHT_BOUND_X), randf_range(TOP_BOUND_Y, BOTTOM_BOUND_Y))
