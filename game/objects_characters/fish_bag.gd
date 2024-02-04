4cd1beb38a293182d378894538a47882a961ba29
rami <rami@pop-os.localdomain>
2024-02-03 19:26:31 -0500

    Updated shelves hierarchy

---
+++ game/objects_characters/fish_bag.gd
@@ -0,0 +1,12 @@
+#extends Node2D
+#
+## Load the scene you want to instantiate
+##var child_scene = preload("res://path_to_your_scene.tscn")
+#
+#func _input(event):
+	#if event is InputEventMouseButton and event.pressed:
+		## Instantiate the scene
+		##var new_child = child_scene.instance()
+		#
+		## Add the new instance to the current scene
+		#add_child(new_child)
